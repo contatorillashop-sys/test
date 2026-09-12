6265b4899fc818a9cdf13e58efe1a64618e715fc
RillaShop <contato.rillashop@gmail.com>
2026-09-12 11:44:07 -0300

    Add function to insert links into table rows

---
+++ insertTexts.js
@@ -0,0 +1,23 @@
+(function () {
+  function inserirTextosNasLinhas(listaTexto) {
+    const textos = listaTexto.split(',').map(t => t.replace('MLB', 'MLB-'));
+    const linhas = document.querySelectorAll('tr.pick_pack_product_row');
+
+    linhas.forEach((linha, index) => {
+      if (index === 0) return;
+
+      const span = linha.querySelector('td div div div span.cell_container_product_warning');
+      if (span) {
+        span.innerHTML = `<a href="https://produto.mercadolivre.com.br/${textos[index - 1]}" target="_blank">${textos[index - 1]}</a>`;
+      }
+    });
+  }
+
+  function getTextoLista() {
+    const div = document.querySelector('div.pick_pack_sale_detail_info:has(> script) + div');
+    return div ? div.textContent.trim() : '';
+  }
+
+  const listaTexto = getTextoLista();
+  inserirTextosNasLinhas(listaTexto);
+})();
