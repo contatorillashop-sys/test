4799e4d6f3db410402b0a1837b26133ab47203f9
RillaShop <contato.rillashop@gmail.com>
2026-09-12 11:21:21 -0300

    Fix anchor tag in product warning span

--- test1.js
+++ test1.js
@@ -7,7 +7,7 @@ function inserirTextosNasLinhas(listaTexto) {
 
     const span = linha.querySelector('td div div div span.cell_container_product_warning');
     if (span) {
-      span.innerHTML = `<a href=https://produto.mercadolivre.com.br/${textos[index - 1]}>${textos[index - 1]}</a>`
+      span.innerHTML = `<a href="https://produto.mercadolivre.com.br/${textos[index-1]" target="_blank"}>${textos[index - 1]}</a>`
     }
   });
 }
